b31853944218fa9bb81bf518c7beff7e3d51b7dd
JohnTheo <ioannis.theodosiou@bluecarpet.eu>
2018-11-26 17:45:45 +0100

    update version, added .npmingnore

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ var path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
     libraryTarget: 'commonjs2'
   },
